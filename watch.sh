551c00bdf5c79c47509e0fcae1a0a733fe2b7372
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-12-22 17:09:29 +0100

    change watch

--- watch.sh
+++ watch.sh
@@ -2,7 +2,7 @@
 TMPFILE=/tmp/watch
 
 while true; do
-  inotifywait -q -e modify,create,delete -r ./{guide,api-reference,dsl-reference,sdk-reference,validation-reference} -o $TMPFILE --format %w && \
+  inotifywait -q -e modify,create,delete -r  /app/{guide,api-reference,dsl-reference,sdk-reference,validation-reference} -o $TMPFILE --format %w && \
     CHANGED=$(cat /tmp/watch | cut -d "/" -f2) && \
     pushd $CHANGED && \
     bundle exec middleman build && \
