8047a20eea67d5391bdf93909360181f95c13a86
Anthony Sendra <asendra@kaliop.com>
2016-12-22 18:19:30 +0100

    add plugin and elasticsearch

--- run.sh
+++ run.sh
@@ -17,6 +17,14 @@ cd /app/validation-reference \
   && bundle exec middleman build \
   && ln -s /app/validation-reference/build /usr/share/nginx/html/validation-reference
 
+cd /app/plugin-reference \
+  && bundle exec middleman build \
+  && ln -s /app/plugin-reference/build /usr/share/nginx/html/plugin-reference
+
+cd /app/elasticsearch-cookbook \
+  && bundle exec middleman build \
+  && ln -s /app/elasticsearch-cookbook/build /usr/share/nginx/html/elasticsearch-cookbook
+
 /watch.sh &
 
 nginx -g 'daemon off;'
