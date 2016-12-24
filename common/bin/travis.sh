2d2311cd4bbf8484ae16c2b88f4643678bc2f1db
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-12-24 11:47:46 +0100

    travis

--- common/bin/travis.sh
+++ common/bin/travis.sh
@@ -5,6 +5,9 @@ set -e
 BINDIR=$(dirname $0)
 SLATEDIRS=(`cat $BINDIR/slatedirs`)
 
+cd $TRAVIS_BUILD_DIR/common/slate
+bundle install
+
 for SLATEDIR in "${SLATEDIRS[@]}"
 do
   echo "Building $SLATEDIR"
