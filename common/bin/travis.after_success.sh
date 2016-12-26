c9183c19ff19f7d8c1d3f6ede36d89c6592e9e48
Anthony Sendra <asendra@kaliop.com>
2016-12-26 16:14:30 +0100

    deploy home directory

--- common/bin/travis.after_success.sh
+++ common/bin/travis.after_success.sh
@@ -29,7 +29,7 @@ deploy () {
   #cp index.html "$DEST/index.html"
 
   # DEPLOY HOME
-  cp -rp $TRAVIS_BUILD_DIR/home/* "$DEST/home/"
+  cp -r $TRAVIS_BUILD_DIR/home "$DEST/home"
 
   cd travis-build
 
