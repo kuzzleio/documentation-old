acff253e7d8b42ce992984ef89440120541e5b02
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-05-22 03:14:39 +0200

    speedup build on PR

--- bin/travis-script.sh
+++ bin/travis-script.sh
@@ -3,6 +3,13 @@
 HOST="http://docs.kuzzle.io"
 VERSION_PATH=$(cat "versions.config.json" | jq -r "map(select(.version_gh_branch == \"${TRAVIS_BRANCH}\")) | .[].version_path")
 
-set -ex
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  set -ex
 
-node index.js --build-host "${HOST}" --build-path "${VERSION_PATH}" --build-compress
+  node index.js --build-host "${HOST}" --build-path "${VERSION_PATH}" --build-compress
+else
+  echo "No build needed. Bye"
+  exit 0
+fi
+
+exit 0
