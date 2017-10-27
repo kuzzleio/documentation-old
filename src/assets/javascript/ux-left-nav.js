dacf637870f72c8bd431c924c14be40217c0ac02
Ballinette <gballini@kaliop.com>
2017-10-27 16:26:04 +0200

    fix tocify scroll

--- src/assets/javascript/ux-left-nav.js
+++ src/assets/javascript/ux-left-nav.js
@@ -1,12 +1,17 @@
 (function leftNavUX() {
+console.log('leftNavUx');
   var $wrapper = $('.tocify-wrapper');
 
+console.log('Wrap length', $wrapper.length);
   if ($wrapper.length !== 0) {
     var $selectedItem = $('.tocify-level-2.tocify-focus', $wrapper);
 
     if ($selectedItem.length === 0) {
       $selectedItem = $('.tocify-focus', $wrapper);
     }
+console.log('selectedItem', $selectedItem);
+console.log('selectedItem Offset', $selectedItem.get(0).offsetTop + 120);
+console.log('wrapper heigth', $wrapper.height());
 
     if ($selectedItem.get(0).offsetTop + 120 > $wrapper.height()) {
       $wrapper.scrollTop(Math.ceil($selectedItem.get(0).offsetTop - $wrapper.height() / 2));
