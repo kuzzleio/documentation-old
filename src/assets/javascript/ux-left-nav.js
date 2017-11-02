74d644999489d8ecf037008bb075db37bfb548f3
Ballinette <gballini@kaliop.com>
2017-11-02 16:13:40 +0100

    cleanup debug console logs

--- src/assets/javascript/ux-left-nav.js
+++ src/assets/javascript/ux-left-nav.js
@@ -1,21 +1,15 @@
 (function leftNavUX() {
-console.log('leftNavUx');
   var $wrapper = $('.tocify-wrapper');
 
-console.log('Wrap length', $wrapper.length);
   if ($wrapper.length !== 0) {
     var $selectedItemList = $('.tocify-level-2.tocify-focus', $wrapper);
 
     if ($selectedItemList.length === 0) {
       $selectedItemList = $('.tocify-focus', $wrapper);
     }
-console.log('selectedItemList', $selectedItemList);
     var $selectedItem = $selectedItemList.get($selectedItemList.length-1)
-console.log('selectedItem Offset', $selectedItem.offsetTop);
-console.log('wrapper heigth', $wrapper.height());
 
     if ($selectedItem.offsetTop + 120 > $wrapper.height()) {
-      console.log(Math.ceil($selectedItem.offsetTop - $wrapper.height() / 2));
       $wrapper.scrollTop(Math.ceil($selectedItem.offsetTop - $wrapper.height() / 2));
     }
   }
