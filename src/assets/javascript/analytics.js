b2fc9f2d4ed7e12fe1a63a4ca8a086bb888d501f
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-04-13 16:54:33 +0200

    move google analytics function in analytics.js

--- src/assets/javascript/analytics.js
+++ src/assets/javascript/analytics.js
@@ -12,7 +12,6 @@
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-67035328-2', 'auto');
-ga('send', 'pageview');
 
 document.addEventListener('turbolinks:load', function(event) {
   if (typeof ga === 'function') {
