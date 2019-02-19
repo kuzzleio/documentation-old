81bf7964b94129cf3b6a2ba67e8011e8f42c7836
njuelle <n.juelle@gmail.com>
2019-02-19 11:30:21 +0100

    [hotfix] GA

--- src/assets/libs/analytics.js
+++ src/assets/libs/analytics.js
@@ -1,22 +1,8 @@
 // google analytics
-(function(i, s, o, g, r, a, m) {
-  i.GoogleAnalyticsObject = r;
-  (i[r] =
-    i[r] ||
-    function() {
-      (i[r].q = i[r].q || []).push(arguments);
-    }),
-  (i[r].l = Number(new Date()));
-  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-})(window, document, 'script', 'www.google-analytics.com/analytics.js', 'ga');
-
-ga('create', 'UA-67035328-2', 'auto');
+gtag('create', 'UA-67035328-2', 'auto');
 
 if (typeof report404ga !== 'undefined' && report404ga) {
-  ga('send', 'event', {
+  gtag('send', 'event', {
     eventCategory: 'documentation-not-found',
     eventAction: document.location.href,
     eventLabel: document.referrer || '',
